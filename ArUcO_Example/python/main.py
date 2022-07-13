5d3b91d32237015846d858bdc1187c780cfdd0dd
lacie-life <00sao00ios00@gmail.com>
2022-07-13 19:26:13 +0700

    [Update]

--- ArUcO_Example/python/main.py
+++ ArUcO_Example/python/main.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 import sys
-from utils import ARUCO_DICT, rotationMatrixToQuaternion3
+from utils import ARUCO_DICT, rotationMatrixToQuaternion3, computeC2MC1
 from calibration_store import load_coefficients
 import argparse
 import time
@@ -19,11 +19,17 @@ def pose_esitmation(frame, aruco_dict_type, matrix_coefficients, distortion_coef
     '''
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
     cv2.imshow("Gray", gray)
+    cv2.waitKey(0)
+
     cv2.aruco_dict = cv2.aruco.Dictionary_get(aruco_dict_type)
     parameters = cv2.aruco.DetectorParameters_create()
 
-    corners, ids, rejected_img_points = cv2.aruco.detectMarkers(gray, cv2.aruco_dict, parameters)
+    corners, ids, rejected_img_points = cv2.aruco.detectMarkers(gray, cv2.aruco_dict,
+                                                                parameters=parameters,
+                                                                cameraMatrix=matrix_coefficients,
+                                                                distCoeff=distortion_coefficients)
 
     # If markers are detected
     if len(corners) > 0:
@@ -35,8 +41,7 @@ def pose_esitmation(frame, aruco_dict_type, matrix_coefficients, distortion_coef
             # Convert rotation vector to rotation matrix
             # https://answers.ros.org/question/314828/opencv-camera-rvec-tvec-to-ros-world-pose/
             # https://docs.opencv.org/4.x/d9/d0c/group__calib3d.html#ga61585db663d9da06b68e70cfbf6a1eac
-            rotationMatrix = any
-            cv2.Rodrigues(rvec, rotationMatrix)
+            rotationMatrix = np.matrix(cv2.Rodrigues(rvec)[0])
 
             print("Rotation Matrix")
             print(rotationMatrix)
@@ -53,14 +58,20 @@ def pose_esitmation(frame, aruco_dict_type, matrix_coefficients, distortion_coef
             # Draw Axis
             cv2.aruco.drawAxis(frame, matrix_coefficients, distortion_coefficients, rvec, tvec, 0.01)
 
-    return frame
+    return frame, rvec, tvec
 
 
 if __name__ == '__main__':
 
     ap = argparse.ArgumentParser()
-    ap.add_argument("--image", required=True, type=str, help="Path to image")
-    ap.add_argument("--file", required=True, type=str, help="Path to calibration matrix")
+    ap.add_argument("--image1", type=str, help="Path to image1",
+                    default="/home/jun/Github/ComputerVisionTools/ArUcO_Example/imgs/center-left/center.jpg")
+    ap.add_argument("--file1", type=str, help="Path to calibration matrix",
+                    default="/home/jun/Github/ComputerVisionTools/ArUcO_Example/center_camera.yaml")
+    ap.add_argument("--image2", type=str, help="Path to image2",
+                    default="/home/jun/Github/ComputerVisionTools/ArUcO_Example/imgs/center-left/left.jpg")
+    ap.add_argument("--file2", type=str, help="Path to calibration matrix",
+                    default="/home/jun/Github/ComputerVisionTools/ArUcO_Example/left_camera.yaml")
     ap.add_argument("--type", type=str, default="DICT_ARUCO_ORIGINAL", help="Type of ArUCo tag to detect")
     args = ap.parse_args()
 
@@ -70,19 +81,45 @@ if __name__ == '__main__':
 
     aruco_dict_type = ARUCO_DICT[args.type]
 
-    print(args.file)
-    K, D = load_coefficients(args.file)
+    K1, D1 = load_coefficients(args.file1)
+    K2, D2 = load_coefficients(args.file2)
 
-    print(K)
-    print(D)
+    print(K1)
+    print(D1)
+
+    print(K2)
+    print(D2)
 
     time.sleep(2.0)
 
-    image = cv2.imread(args.image)
+    print("Camera 1:")
+    image1 = cv2.imread(args.image1)
+    cv2.imshow("Color", image1)
+    cv2.waitKey(0)
 
-    output = pose_esitmation(image, aruco_dict_type, K, D)
+    output1, R1, T1 = pose_esitmation(image1, aruco_dict_type, K1, D1)
 
-    cv2.imshow('Estimated Pose', output)
+    print("Camera 2: ")
+    image2 = cv2.imread(args.image2)
+    cv2.imshow("Color", image2)
+    cv2.waitKey(0)
+
+    output2, R2, T2 = pose_esitmation(image2, aruco_dict_type, K2, D2)
+
+    cv2.imshow('Estimated Pose camera 1', output1)
+    cv2.imshow('Estimated Pose camera 2', output2)
+
+    R_1to2, tvec_1to2 = computeC2MC1(R1, T1, R2, T2)
+
+    print("Transformation camera 1 to camera 2: ")
+    print("Rotation Matrix")
+    print(R_1to2)
+
+    # print("Quaternion")
+    # print(rotationMatrixToQuaternion3(R_1to2))
+
+    print("Translation vector")
+    print(tvec_1to2)
 
     cv2.waitKey(0)
 
---
+++ ArUcO_Example/python/utils.py
@@ -0,0 +1,98 @@
+import cv2
+import numpy as np
+
+ARUCO_DICT = {
+    "DICT_4X4_50": cv2.aruco.DICT_4X4_50,
+    "DICT_4X4_100": cv2.aruco.DICT_4X4_100,
+    "DICT_4X4_250": cv2.aruco.DICT_4X4_250,
+    "DICT_4X4_1000": cv2.aruco.DICT_4X4_1000,
+    "DICT_5X5_50": cv2.aruco.DICT_5X5_50,
+    "DICT_5X5_100": cv2.aruco.DICT_5X5_100,
+    "DICT_5X5_250": cv2.aruco.DICT_5X5_250,
+    "DICT_5X5_1000": cv2.aruco.DICT_5X5_1000,
+    "DICT_6X6_50": cv2.aruco.DICT_6X6_50,
+    "DICT_6X6_100": cv2.aruco.DICT_6X6_100,
+    "DICT_6X6_250": cv2.aruco.DICT_6X6_250,
+    "DICT_6X6_1000": cv2.aruco.DICT_6X6_1000,
+    "DICT_7X7_50": cv2.aruco.DICT_7X7_50,
+    "DICT_7X7_100": cv2.aruco.DICT_7X7_100,
+    "DICT_7X7_250": cv2.aruco.DICT_7X7_250,
+    "DICT_7X7_1000": cv2.aruco.DICT_7X7_1000,
+    "DICT_ARUCO_ORIGINAL": cv2.aruco.DICT_ARUCO_ORIGINAL,
+    "DICT_APRILTAG_16h5": cv2.aruco.DICT_APRILTAG_16h5,
+    "DICT_APRILTAG_25h9": cv2.aruco.DICT_APRILTAG_25h9,
+    "DICT_APRILTAG_36h10": cv2.aruco.DICT_APRILTAG_36h10,
+    "DICT_APRILTAG_36h11": cv2.aruco.DICT_APRILTAG_36h11
+}
+
+
+def aruco_display(corners, ids, rejected, image):
+    if len(corners) > 0:
+        # flatten the ArUco IDs list
+        ids = ids.flatten()
+        # loop over the detected ArUCo corners
+        for (markerCorner, markerID) in zip(corners, ids):
+            # extract the marker corners (which are always returned in
+            # top-left, top-right, bottom-right, and bottom-left order)
+            corners = markerCorner.reshape((4, 2))
+            (topLeft, topRight, bottomRight, bottomLeft) = corners
+            # convert each of the (x, y)-coordinate pairs to integers
+            topRight = (int(topRight[0]), int(topRight[1]))
+            bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
+            bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
+            topLeft = (int(topLeft[0]), int(topLeft[1]))
+
+            cv2.line(image, topLeft, topRight, (0, 255, 0), 2)
+            cv2.line(image, topRight, bottomRight, (0, 255, 0), 2)
+            cv2.line(image, bottomRight, bottomLeft, (0, 255, 0), 2)
+            cv2.line(image, bottomLeft, topLeft, (0, 255, 0), 2)
+            # compute and draw the center (x, y)-coordinates of the ArUco
+            # marker
+            cX = int((topLeft[0] + bottomRight[0]) / 2.0)
+            cY = int((topLeft[1] + bottomRight[1]) / 2.0)
+            cv2.circle(image, (cX, cY), 4, (0, 0, 255), -1)
+            # draw the ArUco marker ID on the image
+            cv2.putText(image, str(markerID), (topLeft[0], topLeft[1] - 10), cv2.FONT_HERSHEY_SIMPLEX,
+                        0.5, (0, 255, 0), 2)
+            print("[Inference] ArUco marker ID: {}".format(markerID))
+        # show the output image
+    return image
+
+
+def rotationMatrixToQuaternion3(m):
+    # q0 = qw
+    t = np.matrix.trace(m)
+    q = np.asarray([0.0, 0.0, 0.0, 0.0], dtype=np.float64)
+
+    if (t.all() > 0):
+        t = np.sqrt(t + 1)
+        q[0] = 0.5 * t
+        t = 0.5 / t
+        q[1] = (m[2, 1] - m[1, 2]) * t
+        q[2] = (m[0, 2] - m[2, 0]) * t
+        q[3] = (m[1, 0] - m[0, 1]) * t
+
+    else:
+        i = 0
+        if (m[1, 1] > m[0, 0]):
+            i = 1
+        if (m[2, 2] > m[i, i]):
+            i = 2
+        j = (i + 1) % 3
+        k = (j + 1) % 3
+
+        t = np.sqrt(m[i, i] - m[j, j] - m[k, k] + 1)
+        q[i] = 0.5 * t
+        t = 0.5 / t
+        q[0] = (m[k, j] - m[j, k]) * t
+        q[j] = (m[j, i] + m[i, j]) * t
+        q[k] = (m[k, i] + m[i, k]) * t
+
+    return q
+
+
+def computeC2MC1(R1, tvec1, R2, tvec2):
+    # c2Mc1 = c2Mo * oMc1 = c2Mo * c1Mo.inv()
+    R_1to2 = R2 * R1.T
+    tvec_1to2 = (R2 * (-R1.T * tvec1)) + tvec2
+    return R_1to2, tvec_1to2
