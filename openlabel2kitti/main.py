fc0fab5ee33d3ab496c313a444ef219daf18c210
lacie-life <00sao00ios00@gmail.com>
2023-12-23 18:23:21 +0900

    [Openlab2Kitti]

--- openlabel2kitti/main.py
+++ openlabel2kitti/main.py
@@ -6,7 +6,8 @@ from scipy.spatial.transform import Rotation as R
 def project_to_image(points_3d, P):
     """Project 3D points to 2D image points."""
     num_points = points_3d.shape[1]
-    points_3d_h = np.vstack([points_3d, np.ones((1, num_points))])
+    # points_3d_h = np.vstack([points_3d, np.ones((1, num_points))])
+    points_3d_h = np.vstack([points_3d[:3, :], np.ones((1, points_3d.shape[1]))])
     points_2d_h = np.dot(P, points_3d_h)
     points_2d = points_2d_h[:2, :] / points_2d_h[2, :]
     return points_2d
@@ -23,7 +24,10 @@ def convert_to_kitti_format(json_file, output_file):
     with open(json_file, 'r') as f:
         data = json.load(f)
 
-    P = np.array(data['openlabel']['streams']['s110_camera_basler_south1_8mm']['stream_properties']['intrinsics_pinhole']['camera_matrix_3x4'])
+    P = np.array(data['openlabel']['streams']['s110_camera_basler_south2_8mm']['stream_properties']['intrinsics_pinhole']['camera_matrix_3x4'])
+    T_lidar_to_camera = np.array(data['openlabel']['coordinate_systems']['s110_camera_basler_south2_8mm']['pose_wrt_parent']['matrix4x4']).reshape(4, 4)
+    image_width = data['openlabel']['streams']['s110_camera_basler_south2_8mm']['stream_properties']['intrinsics_pinhole']['width_px']
+    image_height = data['openlabel']['streams']['s110_camera_basler_south2_8mm']['stream_properties']['intrinsics_pinhole']['height_px']
 
     with open(output_file, 'w') as f_out:
         for frame in data['openlabel']['frames'].values():
@@ -52,7 +56,15 @@ def convert_to_kitti_format(json_file, output_file):
                                           [location[1][0]-w/2, location[1][0]+w/2, location[1][0]+w/2, location[1][0]-w/2, location[1][0]-w/2, location[1][0]+w/2, location[1][0]+w/2, location[1][0]-w/2],
                                           [location[2][0]-h/2, location[2][0]-h/2, location[2][0]-h/2, location[2][0]-h/2, location[2][0]+h/2, location[2][0]+h/2, location[2][0]+h/2, location[2][0]+h/2]])
 
+                    print(points_3d.shape)
+                    print(points_3d)
+                    points_3d = np.vstack([points_3d, np.ones((1, points_3d.shape[1]))])
+                    points_3d = np.dot(T_lidar_to_camera, points_3d)
                     points_2d = project_to_image(points_3d, P)
+
+                    if np.any(points_2d[0, :] < 0) or np.any(points_2d[0, :] > image_width) or np.any(points_2d[1, :] < 0) or np.any(points_2d[1, :] > image_height):
+                        continue  # Skip this object if it's not in the camera's FOV
+
                     bbox_2d = compute_2d_bbox(points_2d)
 
                     f_out.write(f"{object_data['object_data']['type']} 0.0 0 -10.0 {bbox_2d[0]} {bbox_2d[1]} {bbox_2d[2]} {bbox_2d[3]} {h} {w} {l} {location[0][0]} {location[1][0]} {location[2][0]} {yaw} 0\n")
