[Openlab2Kitti] fixed bev projection rotation error

openlabel2kitti/main.py
@@ -65,8 +65,8 @@ def convert_to_kitti_format(json_file, output_file, img, rgb_map):
                     quat_y = float(val[4])
                     quat_z = float(val[5])
                     quat_w = float(val[6])
-                    # rotation_yaw = R.from_quat([quat_x, quat_y, quat_z, quat_w]).as_euler("xyz", degrees=False)[2]
-                    rotation_yaw = R.from_quat([quat_x, quat_y, quat_z, quat_w]).as_euler("zyx", degrees=False)[1]
+                    rotation_yaw = R.from_quat([quat_x, quat_y, quat_z, quat_w]).as_euler("xyz", degrees=False)[2]
+                    # rotation_yaw = R.from_quat([quat_x, quat_y, quat_z, quat_w]).as_euler("zyx", degrees=False)[1]
 
                     location = np.array(
                         [
@@ -120,10 +120,10 @@ def convert_to_kitti_format(json_file, output_file, img, rgb_map):
 
     return img, T_lidar_to_camera
 
-json_file = '/home/lacie/Github/ComputerVisionTools/openlabel2kitti/test_2/1646667315_454719936_s110_lidar_ouster_south.json'
+json_file = '/home/lacie/Github/ComputerVisionTools/openlabel2kitti/test_1/1646667310_053239541_s110_lidar_ouster_south.json'
 output_file = 'kitti_format_data.txt'
-img_path = "/home/lacie/Github/ComputerVisionTools/openlabel2kitti/test_2/1646667315_563367657_s110_camera_basler_south2_8mm.jpg"
-pc_path = "/home/lacie/Github/ComputerVisionTools/openlabel2kitti/test_2/1646667315_454719936_s110_lidar_ouster_south.pcd"
+img_path = "/home/lacie/Github/ComputerVisionTools/openlabel2kitti/test_1/1646667310_055996268_s110_camera_basler_south2_8mm.jpg"
+pc_path = "/home/lacie/Github/ComputerVisionTools/openlabel2kitti/test_1/1646667310_053239541_s110_lidar_ouster_south.pcd"
 
 pc = PointCloud.from_path(pc_path).numpy()
 pc = pc[:, :4]
@@ -178,10 +178,12 @@ targets[:, 6] = torch.atan2(targets[:, 6], targets[:, 7])
 
 print(rgb_map.shape)
 
+print(targets)
+
 for c, x, y, w, l, yaw in targets[:, 1:7].numpy():
     # Draw rotated box
     # print("===============================")
-    # print(c, x, y, w, l, yaw)
+    print(c, x, y, w, l, yaw)
     rgb_map = drawRotatedBox(rgb_map, x, y, w, l, yaw, colors[int(c)], c)
 
 # rgb_map = cv2.rotate(rgb_map, cv2.ROTATE_180)

openlabel2kitti/makeBEVUtils.py
@@ -110,6 +110,7 @@ def build_yolo_target(labels):
             w1 = w / (bc["maxY"] - bc["minY"])
             l1 = l / (bc["maxX"] - bc["minX"])
             target.append([cl, y1, x1, w1, l1, math.sin(float(yaw)), math.cos(float(yaw))])
+            # target.append([cl, y1, x1, w1, l1, yaw, yaw])
 
     return np.array(target, dtype=np.float32)
 
@@ -141,8 +142,10 @@ def get_corners(x, y, w, l, yaw):
 # send parameters in bev image coordinates format
 def drawRotatedBox(img, x, y, w, l, yaw, color, c):
     img_new = img.astype(np.uint8).copy()
+
     cv2.circle(img_new, (int(x), int(y)), radius=2, color=(0, 0, 255), thickness=1)
     cv2.putText(img_new, str(c), (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0, 0, 255), 1)
+
     bev_corners = get_corners(x, y, w, l, yaw)
 
     corners_int = bev_corners.reshape(-1, 1, 2).astype(int)
@@ -162,9 +165,10 @@ def draw_box_in_bev(rgb_map, target):
         cls_id = int(target[j][0])
         x = target[j][1] * BEV_WIDTH
         y = target[j][2] * BEV_HEIGHT
-        w = target[j][3] * BEV_WIDTH
-        l = target[j][4] * BEV_HEIGHT
-        yaw = np.arctan2(target[j][5], target[j][6])
+        w = target[j][4] * BEV_WIDTH
+        l = target[j][5] * BEV_HEIGHT
+        # yaw = np.arctan2(target[j][5], target[j][6])
+        yaw = target[j][6]
         drawRotatedBox(rgb_map, x, y, w, l, yaw, colors[cls_id])
 
 

openlabel2kitti/tools.py
@@ -29,32 +29,10 @@ def camera_to_lidar_box(boxes, L2CTrans):
     for box in boxes:
         x, y, z, h, w, l, ry = box
         (x, y, z), h, w, l, rz = camera_to_lidar(
-            x, y, z, L2CTrans), h, w, l, -ry - np.pi / 2
+            x, y, z, L2CTrans), h, w, l, ry
         # rz = angle_in_limit(rz)
         ret.append([x, y, z, h, w, l, rz])
     return np.array(ret).reshape(-1, 7)
-
-
-def build_yolo_target(labels):
-    bc = boundary
-    target = []
-    for i in range(labels.shape[0]):
-        print(labels[i])
-        cl, x, y, z, h, w, l, yaw = labels[i]
-        # ped and cyc labels are very small, so lets add some factor to height/width
-        l = l + 0.3
-        w = w + 0.3
-        yaw = np.pi * 2 - yaw
-
-        # print(x,y)
-        if (bc["minX"] < x < bc["maxX"]) and (bc["minY"] < y < bc["maxY"]):
-            y1 = (y - bc["minY"]) / (bc["maxY"] - bc["minY"])  # we should put this in [0,1], so divide max_size  80 m
-            x1 = (x - bc["minX"]) / (bc["maxX"] - bc["minX"])  # we should put this in [0,1], so divide max_size  40 m
-            w1 = w / (bc["maxY"] - bc["minY"])
-            l1 = l / (bc["maxX"] - bc["minX"])
-            target.append([cl, y1, x1, w1, l1, math.sin(float(yaw)), math.cos(float(yaw))])
-
-    return np.array(target, dtype=np.float32)
 
 
 class Object3d(object):